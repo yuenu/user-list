a44d7f5597889907fafc7f13cb279ad5c21f5a99
yuenu <miraiku82@gmail.com>
2022-03-22 18:15:40 +0800

    feat: styling background

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -9,7 +9,7 @@ defineProps<{
 </script>
 
 <template lang="pug">
-div(:class="['w-full h-auto cursor-pointer', isLoading && 'animate-pulse', displayType === 'row' && 'flex items-center justify-between w-1/2 mx-auto']")
+div(:class="['w-full h-auto cursor-pointer', isLoading && 'animate-pulse', displayType === 'row' && 'flex flex-col sm:flex-row items-center justify-between w-full lg:w-1/2 mx-auto']")
   img(:src=" user.picture.large" :class="['object-cover w-full max-w-full', displayType !== 'column' && 'h-[200px] w-[200px]']")
   span(v-if="displayType === 'row'" class="text-3xl") {{ user.name.first }}  {{ user.name.last }}
   
