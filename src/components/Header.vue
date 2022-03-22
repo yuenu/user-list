816a4b270bc4ef547a550f70fc74cf13e800aca4
yuenu <miraiku82@gmail.com>
2022-03-22 18:15:10 +0800

    feat: add header title

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,7 +15,10 @@ watch(selected, async (newSelected, _oldSelected) => {
 </script>
 
 <template lang="pug">
-header(class="flex justify-end w-full p-10")
+header(class="flex items-center justify-between w-full p-4 sm:p-10")
+	span(class="relative inline-block text-xl sm:text-2xl md:text-5xl lg:text-6xl before:block before:absolute before:-inset-1 before:-skew-y-2 before:bg-pink-500")
+		span(class="relative text-white") FOUND RANDOM
+	//- div(class="text-xl sm:text-2xl md:text-5xl lg:text-6xl before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500")
 	div(class="flex items-center w-ful gap-x-3")
 		div(id='select')
 			select(class="p-2 border rounded-sm border-gray" v-model="selected")
